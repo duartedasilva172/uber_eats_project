ae06763b520b7ae0b3f249f3d8e4a4d7ce473df8
duartedasilva172 <duartedasilva@Duartes-MBP.lan>
2025-06-12 19:30:00 -0400

    Update

---
+++ uber_eats_data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE uber_eats_sessions (
+    date DATE,
+    start_time DATETIME,
+    end_time DATETIME,
+    shift_length_hrs FLOAT,
+    device_used VARCHAR(256)
+)
+
+INSERT INTO uber_eats_sessions (
+    
+)
